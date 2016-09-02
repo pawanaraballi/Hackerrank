6d169de37f6517cfe3a4fdd3939d40bb0886403f
Pawan Araballi <araballi.pawan@gmail.com>
2016-09-02 12:58:35 -0400

    lcd-bst

---
+++ DataStructures/Trees/lowestcommondivisor-bst.java
@@ -0,0 +1,12 @@
+/* Lowest Common Divisor - Binary Search Tree */
+
+static Node lca(Node root,int v1,int v2)
+    {
+        if(v1 < root.data && v2 < root.data){
+            return lca(root.left,v1,v2);
+        }
+        else if(v1 > root.data && v2 > root.data){
+            return lca(root.right,v1,v2);
+        }
+        return root;
+    }
